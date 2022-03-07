ad50f5a5cba348f730528cf569a3942b647e62da
jason <jasonprogrammer91@gmail.com>
2022-03-07 11:24:40 +0100

    first

---
+++ client/app.js
@@ -0,0 +1,16 @@
+
+
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+
+
+const App=()=>{
+
+    return (
+       <div>
+           Hello! We are setting up a real world application!
+       </div>
+        )
+}
+
+export default hot(App) ;
---
+++ readme.md
@@ -0,0 +1,7 @@
+
+
+
+https://blog.bitsrc.io/an-efficient-way-to-set-up-a-mern-stack-application-12fd87fad90d
+
+
+https://github.com/Thomas-Max99/mern-app-setup
